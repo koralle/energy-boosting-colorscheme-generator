8f904dd185c58efad0a16aa11444f707728a9722
koralle <koralle@koralle-mgmg.com>
2026-01-19 03:05:25 +0900

    feat: fix typo

--- panda.config.ts
+++ panda.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   outdir: "styled-system",
 
   // Global CSS
-  cssVarsRoot: ":where(:where, :where, :where))",
+  cssVarsRoot: ":where(:where, :where, :where)",
   globalCss: {
     body: {
       bg: "white",
